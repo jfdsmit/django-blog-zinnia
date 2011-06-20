ae46adc3b08f5ae8074af366276be2e4a73a35dd
Jens de Smit <jfdsmit@gmail.com>
2011-06-20 11:04:30 +0200

    bughunting

--- zinnia/urls/discussions.py
+++ zinnia/urls/discussions.py
@@ -1,7 +1,7 @@
 """Urls for the Zinnia discussions"""
 from django.conf.urls.defaults import url
 from django.conf.urls.defaults import patterns
-import views
+import zinnia.views
 
 urlpatterns = patterns('',
                        url(r'^success/$', 'views.comment_posted',
--- zinnia/views/discussions.py
+++ zinnia/views/discussions.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 from django.contrib.comments.models import Comment
 
 def comment_posted(request):
-    comment = Comment.objects.get(request["c"])
+    comment = Comment.objects.get(pk=request["c"])
     return {"ok": True,
             "id": request.GET.get("c"),
             "comment": render(request, "blog/comment_template.html", {"id": comment.id, "comment": comment.comment})
